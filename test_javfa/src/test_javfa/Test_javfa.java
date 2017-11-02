70a70b2371e03540ed91200280683ed5ac2215b7
Frozxt <Frozxt@gmail.com>
2017-11-02 09:05:38 -0400

    se coloco otro print

--- test_javfa/src/test_javfa/Test_javfa.java
+++ test_javfa/src/test_javfa/Test_javfa.java
@@ -9,6 +9,8 @@ public class Test_javfa {
 
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        
+        System.out.println("los saluda el sr papita");
     }
     
 }
