e1612bed7589f5e0f3d2b8a23823bb3d271637cd
Frozxt <Frozxt@gmail.com>
2017-11-02 08:49:03 -0400

    se crea la clase main.jafva con hola mundo

---
+++ test_javfa/src/test_javfa/Test_javfa.java
@@ -0,0 +1,14 @@
+
+package test_javfa;
+
+/**
+ *
+ * @author jcatrileo
+ */
+public class Test_javfa {
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+    
+}
